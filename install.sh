b60c5f2548d4ab4066c774f5a3aa07c1d9e2e20c
Vlad1ckPON <kharkovervladimir@gmail.com>
2024-05-04 22:35:29 +0300

    now will install AstroNvim

--- install.sh
+++ install.sh
@@ -3,7 +3,7 @@
 echo 'Installing all apps...'
 
 # установка приложений, которые есть в репозитории pacman
-sudo pacman -S --noconfirm git polybar pavucontrol binutils gcc dkms libglvnd  networkmanager bluez bluez-utils nitrogen rofi alacritty nvidia-settings nvidia thunar bspwm sxhkd xdg-user-dirs
+sudo pacman -S --noconfirm git polybar pavucontrol ripgrep binutils gcc npm dkms libglvnd  networkmanager bluez bluez-utils nitrogen rofi alacritty nvidia-settings nvidia thunar bspwm sxhkd xdg-user-dirs
 mkdir -p /home/$USER/.config/bspwm/
 mkdir -p /home/$USER/.config/sxhkd/
 cp /usr/share/doc/bspwm/examples/bspwmrc /home/$USER/.config/bspwm/
@@ -21,6 +21,12 @@ cd
 
 echo 'yay installed successfully, start installing aur apps'
 yay -S --noconfirm telegram-desktop visual-studio-code-bin chromium font-manager cava cmatrix
+
+echo 'установка и настройка astranvim'
+npm install node pyright tree-sitter-cli
+git clone --depth 1 https://github.com/AstroNvim/template ~/.config/nvim
+# remove template's git connection to set up your own later
+rm -rf ~/.config/nvim/.git
 
 echo 'aur installed successfully, start cloning config repo'
 cd /home/$USER/Documents/
